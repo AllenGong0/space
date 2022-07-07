03af8516796a93d6e4cb90893a322dca52caa921
gonglixin980926 <1480059245@qq.com>
2022-07-07 14:58:23 +0800

    feat: 可编辑表格

---
+++ src/pages/Contenteditable/index.tsx
@@ -0,0 +1,10 @@
+const ContentEditAble = () => {
+    return (
+      <div contentEditable={true} style={{ background: "aqua" }}>
+        <span>
+          <h1>测试文案</h1>
+        </span>
+      </div>
+    )
+}
+export default ContentEditAble
